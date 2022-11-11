e903a974a452a07b52548d1b13985fbe21763040
bobxli <bobxli@tencent.com>
2022-11-12 00:01:37 +0800

    1

--- formula/pivot-lang.rb
+++ formula/pivot-lang.rb
@@ -24,6 +24,10 @@ class PivotLang < Formula
     bin.install "./plc"
     system "bash", "-c", "echo 'export KAGARI_LIB_ROOT=$(brew --prefix pivot-lang)/lib/planglib' >> ~/.bash_profile"
     system "bash", "-c", "echo 'export PL_ROOT=$(brew --prefix pivot-lang)/lib' >> ~/.bash_profile"
-    system "bash", "-c", "echo '\033[0;32minstall success, please restart your terminal!\033[0m'"
+    system "\033[0;32m安装完成，请运行下方命令添加环境变量\033[0m"
+    system "    echo 'export KAGARI_LIB_ROOT=$(brew --prefix pivot-lang)/lib/planglib' >> ~/.bash_profile"
+    system "    echo 'export PL_ROOT=$(brew --prefix pivot-lang)/lib' >> ~/.bash_profile"
+    system "    source ~/.bash_profile"
+    system "\033[0;32m如果您已经打开了vsc，可能需要重启它来应用环境变量更改\033[0m"
   end
 end
