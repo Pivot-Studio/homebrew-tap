4890181a0eda5132ffeb762fb78ffc3a3afa1bd1
pivot-lang authors <1769712655@qq.com>
2023-02-05 16:23:52 +0000

    "Brew formula update for pivot-lang version v0.1.232"

--- formula/pivot-lang.rb
+++ formula/pivot-lang.rb
@@ -5,15 +5,15 @@
 class PivotLang < Formula
   desc "Pivot-lang 二次元指定编程语言"
   homepage "https://github.com/Pivot-Studio/pivot-lang"
-  url "https://github.com/Pivot-Studio/pivot-lang/archive/v0.1.231.tar.gz"
-  sha256 "8c8c04dbafe32ac6440f87b96c466892e49443d9e70e0b55e029a44312d81fe2"
+  url "https://github.com/Pivot-Studio/pivot-lang/archive/v0.1.232.tar.gz"
+  sha256 "b01ab7e6be0affbaa412e17648d494cfc06ab8cbcea7442ea4531d94b927a2b6"
   license "MIT"
 
   on_macos do
 
     on_arm do
-      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.231/pivot-lang-0.1.231-darwin-arm64.tar.gz"
-      sha256 "7662e996db02f0e1e934689562a3ec6d4456fd6b9586febb3b85cb6f675d11cd"
+      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.232/pivot-lang-0.1.232-darwin-arm64.tar.gz"
+      sha256 "4194fab09227d91c61b67df5cc4653fb2fd4b1832dfe2881031da8e2dc4b09e4"
     end
   end
 
