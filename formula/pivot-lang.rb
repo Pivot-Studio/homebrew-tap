b298fe2283d78453d972eada87aea71733759986
pivot-lang authors <1769712655@qq.com>
2023-04-20 06:06:39 +0000

    "Brew formula update for pivot-lang version v0.1.308"

--- formula/pivot-lang.rb
+++ formula/pivot-lang.rb
@@ -5,19 +5,19 @@
 class PivotLang < Formula
   desc "Pivot-lang 二次元指定编程语言"
   homepage "https://github.com/Pivot-Studio/pivot-lang"
-  url "https://github.com/Pivot-Studio/pivot-lang/archive/v0.1.306.tar.gz"
-  sha256 "fb4220c6fcada4ae18f0fc58d270270de5f52625d7d7f1fa168a526307372ecb"
+  url "https://github.com/Pivot-Studio/pivot-lang/archive/v0.1.308.tar.gz"
+  sha256 "77dc7305d4a4bb38c9e576cdb678651114bb5bfe9e8a5527a7ffde1e24f7c7b4"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.306/pivot-lang-0.1.306-darwin-amd64.tar.gz"
-      sha256 "ae56ffff00e99d9d3617bc19536e70c8813c426d0c825ea6f669d1f5805c92c1"
+      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.308/pivot-lang-0.1.308-darwin-amd64.tar.gz"
+      sha256 "ca5dc4f364e1c8b0f1eb0ebf936ee86821b756c38eb3125a30e35b29bf314543"
     end
 
     on_arm do
-      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.306/pivot-lang-0.1.306-darwin-arm64.tar.gz"
-      sha256 "123b8f9af7c3369bf8cccf6bec5881e59c8bce37bfb6aec89108b2fd94a00bcf"
+      url "https://github.com/Pivot-Studio/pivot-lang/releases/download/v0.1.308/pivot-lang-0.1.308-darwin-arm64.tar.gz"
+      sha256 "b4aae3601d6332e81bc40b73e335afb688d07cab392e73c5ec2af9654120aaa0"
     end
   end
 
